0f0f46830f84f0f32c983fc401bcabf25ae689f0
MKS-students <mks.students@hackreactor.com>
2015-07-30 10:30:05 -0500

    completed stack for functional-shared pattern

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,38 @@
+
+//constructor function
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+
+  //object being constructed
+  var newStack = {
+    storage: {},
+
+    //uses "stackSize to avoid conflict with 'size' in stackMethods"
+    stackSize: 0
+  };
+
+  //adds new methods to constructed object
+  _.extend(newStack, stackMethods);
+
+  return newStack;
 };
 
-var stackMethods = {};
+var stackMethods = {
 
+//various methods to be applied to constructed object
+  push: function(value){
+    this.storage[this.stackSize] = value;
+    this.stackSize++
+  },
 
+  pop: function(){
+    this.stackSize && this.stackSize--;
+    var result = this.storage[this.stackSize];
+    delete this.storage[this.stackSize];
+    return result;
+  },
+
+  size: function(){
+    return this.stackSize;
+  }
+
+};

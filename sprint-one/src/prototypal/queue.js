75bb25fca856aa067639fd4323684b9851db5a51
MKS-students <mks.students@hackreactor.com>
2015-07-30 10:59:23 -0500

    completed prototypal and pseudoclassical stack and queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,32 @@
-var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+var Queue = function(){
+
+  var newQueue = Object.create(queueMethods);
+  newQueue.storage = {};
+  newQueue.queueSize = 0;
+
+  return newQueue;
 };
 
-var queueMethods = {};
 
+var queueMethods = {
 
+  enqueue: function(value){
+    for ( var i = this.queueSize; i > 0; i-- ) {
+      this.storage[i] = this.storage[i -1];
+    }
+    this.storage[0] = value;
+    this.queueSize++;
+  },
+
+  dequeue: function(){
+    this.queueSize && this.queueSize--;
+    var result = this.storage[this.queueSize];
+    delete this.storage[this.queueSize];
+    return result;
+  },
+
+  size: function(){
+    return this.queueSize;
+  }
+
+};
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,33 @@
+
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+
+  //creates new object with prototype stackMethods (and its associated methods)
+  var newStack = Object.create(stackMethods);
+  newStack.storage = {};
+  newStack.stackSize = 0;
+
+  return newStack;
+  };
+
+var stackMethods = {
+
+  push: function(value){
+    this.storage[this.stackSize] = value;
+    this.stackSize++
+  },
+
+  pop: function(){
+    this.stackSize && this.stackSize--;
+    var result = this.storage[this.stackSize];
+    delete this.storage[this.stackSize];
+    return result;
+  },
+
+  size: function(){
+    return this.stackSize;
+  }
+
 };
 
-var stackMethods = {};
 
 

894bda39b1ead1e320819bdf19f7a7beb9a87811
MKS-students <mks.students@hackreactor.com>
2015-08-01 15:57:15 -0500

    refactored hashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,60 +3,30 @@ var HashTable = function(){
   this._storage = LimitedArray(this._limit);
 };
 
-//we need to learn how to make buckets
-
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
-  //bucket assignment ==> storage[index] OR empty array
   var bucket = this._storage.get(i) || [];
-
-  //Into bucket, push both key and value arguments
-  //Why?
-  //if there's more than one item in the bucket,
-  //given its key, we can fetch the value
   bucket.push([k,v]);
-
-  //sets bucket at the storage[index]
   this._storage.set(i,bucket);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
-  //bucket assignment ==> storage[index]
   var bucket = this._storage.get(i);
-
-  //iterating over the length of the bucket
   for (var j = 0; j < bucket.length; j++) {
-
-    //checks each array in the bucket
-    //comparing first element to the key we want to retrieve
     if (bucket[j][0] === k) {
-
-      //returns value to the corresponding key
       return bucket[j][1];
     }
   }
+  return null;
 };
 
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
-  //bucket assignment
   var bucket = this._storage.get(i);
-
-  //iterates over length of bucket
   for (var j = 0; j < bucket.length; j++) {
-
-    //checks each array in the bucket
-    //comparing first element to the key we want to retrieve
     if (bucket[j][0] === k) {
-
-      //sets corresponding value to null
-      bucket[j][1] = null;
-
-      //sets bucket back into storage[index]
+      bucket.splice(j,1);
       this._storage.set(i,bucket);
     }
   }

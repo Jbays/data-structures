a21da1f70ba2adbf8aba185762b2d95590511670
MKS-students <mks.students@hackreactor.com>
2015-07-31 19:41:55 -0500

    completed set and began working on hashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,17 +3,32 @@ var HashTable = function(){
   this._storage = LimitedArray(this._limit);
 };
 
+//we need to learn how to make buckets
+
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(i) || [];
+  bucket.push([k,v]);
+  this._storage.set(i,bucket);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  var bucket = this._storage.get(i);
+  _.each(bucket, function(item) {
+    if (item[0] === k) {
+      return item[1];
+    }
+  });
 };
 
 HashTable.prototype.remove = function(k){
-
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  _.each(bucket, function(item) {
+    if (item[0] === k) {
+      this._storage.set(i,[k,null]);
+    }
+  });
 };
 
 

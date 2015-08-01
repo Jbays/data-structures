8d0bfd3c3e2c8ce5634df6d0b101302216836192
MKS-students <mks.students@hackreactor.com>
2015-08-01 16:24:21 -0500

    refactored set to use the has function

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,24 +1,29 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = [];
+  set._storage = LimitedArray(this._limit);
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
-  this._storage.push(item);
+  var i = getIndexBelowMaxForKey(item, this._limit);
+  this._storage.set(i, item);
 };
 
 setPrototype.contains = function(item){
-  return !!(this._storage.indexOf(item) > -1);
+  var i = getIndexBelowMaxForKey(item, this._limit);
+  return !!this._storage.get(i);
 };
 
 setPrototype.remove = function(item){
-  this._storage.splice(this._storage.indexOf(item),1);
+  var i = getIndexBelowMaxForKey(item, this._limit);
+  this._storage.set(i, null);
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
-
+add O(1)
+contains O(1)
+remove O(1)
  */

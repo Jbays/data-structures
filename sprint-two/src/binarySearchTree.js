60a3d11f5f453582d12a4766b2b8f451546a9764
MKS-students <mks.students@hackreactor.com>
2015-08-01 11:28:20 -0500

    completed binarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,67 @@
 var BinarySearchTree = function(value){
+  var binarySearchTree = Object.create(binarySearchTreeMethods);
+  binarySearchTree.value = value;
+  binarySearchTree.left = null;
+  binarySearchTree.right = null;
+  return binarySearchTree;
+};
+
+var binarySearchTreeMethods = {
+
+  insert: function(v) {
+    var node = BinarySearchTree(v);
+    if (v > this.value) {
+      if (!this.right) {
+        this.right = node;
+      } else {
+        this.right.insert(v);
+      }
+    } else if (v < this.value) {
+      if (!this.left) {
+        this.left = node;
+      } else {
+        this.left.insert(v);
+      }
+    }
+  },
+
+  //accepts a value and returns a boolean reflecting whether or not the value is contained in the tree.
+  contains: function(v) {
+    var doesItContain = false;
+    var checkNode = function(node) {
+      if (v === node.value) {
+        doesItContain = true;
+      } else if (v > node.value) {
+        if (v === node.right.value) {
+          doesItContain = true;
+        } else {
+          checkNode(node.right.value);
+        }
+      } else if (v < node.value) {
+        if (v === node.left.value) {
+          doesItContain = true;
+        } else {
+          checkNode(node.left.value);
+        }
+      }
+    };
+    checkNode(this);
+    return doesItContain;
+  },
+
+
+//A .depthFirstLog() method, which accepts a callback and executes it on every value contained in the tree.
+  depthFirstLog: function(cb, node) {
+    node = node || this;
+    cb(node.value);
+    if (node.left) {
+      this.depthFirstLog(cb, node.left);
+    }
+    if (node.right) {
+      this.depthFirstLog(cb, node.right);
+    }
+  }
+
 };
 
 
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,16 +7,34 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+
+  //bucket assignment ==> storage[index] OR empty array
   var bucket = this._storage.get(i) || [];
+
+  //Into bucket, push both key and value arguments
+  //Why?
+  //if there's more than one item in the bucket,
+  //given its key, we can fetch the value
   bucket.push([k,v]);
+
+  //sets bucket at the storage[index]
   this._storage.set(i,bucket);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
+
+  //bucket assignment ==> storage[index]
   var bucket = this._storage.get(i);
+
+  //iterating over the length of the bucket
   for (var j = 0; j < bucket.length; j++) {
+
+    //checks each array in the bucket
+    //comparing first element to the key we want to retrieve
     if (bucket[j][0] === k) {
+
+      //returns value to the corresponding key
       return bucket[j][1];
     }
   }
@@ -24,10 +42,21 @@ HashTable.prototype.retrieve = function(k){
 
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
+
+  //bucket assignment
   var bucket = this._storage.get(i);
+
+  //iterates over length of bucket
   for (var j = 0; j < bucket.length; j++) {
+
+    //checks each array in the bucket
+    //comparing first element to the key we want to retrieve
     if (bucket[j][0] === k) {
+
+      //sets corresponding value to null
       bucket[j][1] = null;
+
+      //sets bucket back into storage[index]
       this._storage.set(i,bucket);
     }
   }
@@ -37,4 +66,7 @@ HashTable.prototype.remove = function(k){
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ hashtable Insert ==> O(1)
+ hashtable Retrieve ==> O(1) (As long as we hold the bucket size constant)
+ hashtable Remove ==> O(1)
  */
